c707f020496240b0750b85fb99ef2f0eb2777680
Amos Bastian <amosbastian@gmail.com>
2021-08-09 21:43:22 +0200

    feat: fix i18n blog & site, add common.json and signin.json

--- apps/blog/i18n.js
+++ apps/blog/i18n.js
@@ -5,7 +5,7 @@ module.exports = {
     "*": ["common"],
   },
   loadLocaleFrom: async (locale, namespace) => {
-    const i18nShared = await import(`../../libs/shared/i18n/src/lib/${locale}/${namespace}`).then((m) => m.default);
+    const i18nShared = await import(`../../libs/shared/i18n/src/lib/${locale}/common`).then((m) => m.default);
     const i18nBlog = await import(`../../libs/blog/i18n/src/lib/${locale}/${namespace}`).then((m) => m.default);
 
     return { ...i18nShared, ...i18nBlog };
