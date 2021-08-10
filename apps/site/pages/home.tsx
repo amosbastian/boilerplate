96c9f0e332c73b8c34d0a9ec0891c36f597d1d0d
Amos Bastian <amosbastian@gmail.com>
2021-08-10 22:00:41 +0200

    feat(site): update /home to redirect to /signin when not logged in

--- apps/site/pages/home.tsx
+++ apps/site/pages/home.tsx
@@ -1,6 +1,26 @@
-import { Container, getLayout } from "@boilerplate/shared/ui";
-import useTranslation from "next-translate/useTranslation";
+import { Container } from "@boilerplate/shared/ui";
+import { getLayout } from "@boilerplate/site/ui";
+import type { GetServerSidePropsContext } from "next";
+import { getSession } from "next-auth/client";
 import { NextSeo } from "next-seo";
+import useTranslation from "next-translate/useTranslation";
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
 
 export default function Home() {
   const { t } = useTranslation("home");
