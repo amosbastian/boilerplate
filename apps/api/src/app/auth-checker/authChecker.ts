5acdbf434f99e268d467cf9be307220438521fda
Amos Bastian <amosbastian@gmail.com>
2021-08-16 23:07:56 +0200

    feat: add authChecker function

---
+++ apps/api/src/app/auth-checker/authChecker.ts
@@ -0,0 +1,21 @@
+import { Context } from "@boilerplate/shared/types";
+import { AuthChecker } from "type-graphql";
+
+export const authChecker: AuthChecker<Context> = ({ context: { user } }, roles) => {
+  if (roles.length === 0) {
+    return Boolean(user);
+  }
+
+  if (!user) {
+    return false;
+  }
+
+  const userRoles = (user.roles ?? []).map((role) => role.name.toUpperCase());
+  const roleSet = new Set(roles.map((role) => role.toUpperCase()));
+
+  if (user.roles && userRoles.some((role) => roleSet.has(role))) {
+    return true;
+  }
+
+  return false;
+};
