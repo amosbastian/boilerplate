6cd6f7d95bc8140917e599999fbac814c80cd089
Amos Bastian <amosbastian@gmail.com>
2021-08-24 21:43:44 +0200

    feat: update product resolver with pricing relationship

--- libs/api/data-access/src/lib/product/ProductResolver.ts
+++ libs/api/data-access/src/lib/product/ProductResolver.ts
@@ -1,6 +1,6 @@
 import { Context } from "@boilerplate/shared/types";
-import { FindManyProductArgs, Product } from "@generated/type-graphql";
-import { Args, Ctx, Query, Resolver } from "type-graphql";
+import { FindManyProductArgs, Price, Product } from "@generated/type-graphql";
+import { Args, Ctx, FieldResolver, Query, Resolver, Root } from "type-graphql";
 
 @Resolver(() => Product)
 export class ProductResolver {
@@ -8,5 +8,16 @@ export class ProductResolver {
   async products(@Ctx() { prisma }: Context, @Args() args: FindManyProductArgs): Promise<Product[]> {
     console.log("products");
     return prisma.product.findMany(args);
+  }
+
+  @FieldResolver(() => Price, { nullable: false })
+  async price(@Root() product: Product, @Ctx() { prisma }: Context): Promise<Price | null> {
+    return prisma.product
+      .findUnique({
+        where: {
+          id: product.id,
+        },
+      })
+      .price({});
   }
 }
