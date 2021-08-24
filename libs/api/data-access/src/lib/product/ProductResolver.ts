2196af49ca90952b8911c0a498ba88387d433365
Amos Bastian <amosbastian@gmail.com>
2021-08-24 23:32:23 +0200

    fix(api): update product resolver price to oprices field resolver

--- libs/api/data-access/src/lib/product/ProductResolver.ts
+++ libs/api/data-access/src/lib/product/ProductResolver.ts
@@ -1,23 +1,26 @@
 import { Context } from "@boilerplate/shared/types";
-import { FindManyProductArgs, Price, Product } from "@generated/type-graphql";
+import { FindManyProductArgs, Price, Product, ProductPricesArgs } from "@generated/type-graphql";
 import { Args, Ctx, FieldResolver, Query, Resolver, Root } from "type-graphql";
 
 @Resolver(() => Product)
 export class ProductResolver {
   @Query(() => [Product], { nullable: false })
   async products(@Ctx() { prisma }: Context, @Args() args: FindManyProductArgs): Promise<Product[]> {
-    console.log("products");
     return prisma.product.findMany(args);
   }
 
-  @FieldResolver(() => Price, { nullable: false })
-  async price(@Root() product: Product, @Ctx() { prisma }: Context): Promise<Price | null> {
+  @FieldResolver(() => [Price], { nullable: false })
+  async prices(
+    @Root() product: Product,
+    @Ctx() { prisma }: Context,
+    @Args() args: ProductPricesArgs,
+  ): Promise<Price[]> {
     return prisma.product
       .findUnique({
         where: {
           id: product.id,
         },
       })
-      .price({});
+      .prices(args);
   }
 }
