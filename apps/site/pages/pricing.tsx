52b915adddacc364ebe5c6405e1267528c5a7666
Amos Bastian <amosbastian@gmail.com>
2021-08-26 21:57:20 +0200

    feat: add pricing page

--- apps/site/pages/pricing.tsx
+++ apps/site/pages/pricing.tsx
@@ -1,6 +1,17 @@
-import { getLayout } from "@boilerplate/shared/ui";
+import { getLayout, Section, Container } from "@boilerplate/shared/ui";
 import { NextSeo } from "next-seo";
 import useTranslation from "next-translate/useTranslation";
+import { Heading, Text } from "@chakra-ui/react";
+import { ProductCards } from "@boilerplate/site-feature";
+import type { Question } from "next-seo/lib/jsonld/faqPage";
+import { FaqSection } from "@boilerplate/site/ui";
+
+const FREQUENTLY_ASKED_QUESTIONS: Question[] = [
+  { questionName: "pricing:question-1", acceptedAnswerText: "pricing:answer-1" },
+  { questionName: "pricing:question-2", acceptedAnswerText: "pricing:answer-2" },
+  { questionName: "pricing:question-3", acceptedAnswerText: "pricing:answer-3" },
+  { questionName: "pricing:question-4", acceptedAnswerText: "pricing:answer-4" },
+];
 
 export default function Pricing() {
   const { t } = useTranslation("pricing");
@@ -8,6 +19,31 @@ export default function Pricing() {
   return (
     <>
       <NextSeo title={t("meta-title")} />
+      <Section variant="transparent">
+        <Container>
+          <Heading textAlign="center" mb={{ base: 4, sm: 6, md: 8 }}>
+            {t("heading")}
+          </Heading>
+          <Text mb={16} variant="textSecondary" fontSize="lg" textAlign="center">
+            {t("subtitle")}
+          </Text>
+          <ProductCards />
+        </Container>
+      </Section>
+      <Section>
+        <Container maxW="container.md" m="0 auto">
+          <FaqSection heading={t("faq:heading")} faq={FREQUENTLY_ASKED_QUESTIONS} />
+        </Container>
+      </Section>
+      <Section variant="transparent">
+        {/* <CtaCard backgroundColor={cardBackgroundColor} heading={t("cta-heading")} subtitle={t("cta-subtitle")}>
+          <ChakraLink>
+            <Button w="100%" colorScheme="primary" href="mailto:support@frontend.com">
+              {t("cta-button-text")}
+            </Button>
+          </ChakraLink>
+        </CtaCard> */}
+      </Section>
     </>
   );
 }
