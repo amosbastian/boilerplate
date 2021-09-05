f8dd424d446142c8d998873b7d978bd9f0508f02
Amos Bastian <amosbastian@gmail.com>
2021-09-06 01:02:39 +0200

    feat(site): update hero with image

--- libs/site/ui/src/lib/hero/Hero.tsx
+++ libs/site/ui/src/lib/hero/Hero.tsx
@@ -1,6 +1,7 @@
-import { Container } from "@boilerplate/shared/ui";
+import { Section } from "@boilerplate/shared/ui";
 import { Flex, Heading, Icon } from "@chakra-ui/react";
 import useTranslation from "next-translate/useTranslation";
+import NextImage from "next/image";
 import NextLink from "next/link";
 import { RiArrowRightLine } from "react-icons/ri";
 import { GradientButton } from "../gradient-button/GradientButton";
@@ -9,28 +10,37 @@ export function Hero() {
   const { t } = useTranslation("index");
 
   return (
-    <Container>
-      <Flex direction="column" justify="center" py={{ base: 16, md: 28 }} align="center">
-        <Heading as="h1" fontSize={{ base: "4xl", md: "7xl" }} mb={8} maxW="2xl" textAlign="center">
+    <Section
+      containerProps={{
+        display: "flex",
+        gridColumnGap: { md: 10, xl: 20 },
+        gridRowGap: { base: 8, md: 16 },
+        flexDirection: { base: "column", lg: "row" },
+        justifyContent: "space-between",
+      }}
+      variant="transparent"
+      py={{ base: 20, lg: 44 }}
+    >
+      <Flex
+        direction="column"
+        justifyContent="center"
+        alignItems={{ base: "center", lg: "flex-start" }}
+        maxW={{ base: "100%", lg: "500px" }}
+      >
+        <Heading as="h1" fontSize={{ base: "4xl", md: "7xl" }} mb={6} maxW="2xl">
           {t("hero-heading")}
         </Heading>
-        <Heading
-          variant="textSecondary"
-          as="h2"
-          fontSize={{ base: "md", md: "2xl" }}
-          mb={16}
-          maxW="xl"
-          textAlign="center"
-        >
+        <Heading variant="secondary" as="h2" fontSize={{ base: "md", md: "2xl" }} mb={{ base: 8, md: 16 }}>
           {t("hero-subtitle")}
         </Heading>
-        <NextLink href="/signin">
-          <GradientButton size="lg" rightIcon={<Icon as={RiArrowRightLine} />}>
+        <NextLink href="/signin" passHref>
+          <GradientButton as="a" size="lg" rightIcon={<Icon as={RiArrowRightLine} />}>
             {t("hero-cta")}
           </GradientButton>
         </NextLink>
       </Flex>
-    </Container>
+      <NextImage src="http://placekitten.com/g/900/500" alt={t("hero-image-alt-text")} width={900} height={500} />
+    </Section>
   );
 }
 
