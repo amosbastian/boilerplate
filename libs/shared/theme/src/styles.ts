1977ea1d90ab64fa6c8b8871ce67db0ffa5c32fa
Amos Bastian <amosbastian@gmail.com>
2021-09-08 23:52:24 +0200

    feat(theme): make scrollbar always visible, add styling

--- libs/shared/theme/src/styles.ts
+++ libs/shared/theme/src/styles.ts
@@ -9,6 +9,16 @@ export const styles: Styles = {
       height: "-webkit-fill-available",
       background: mode("white", "gray.900")(props),
       fontSize: "0.875rem",
+      overflowY: "scroll",
+      "&::-webkit-scrollbar": {
+        width: "1em",
+      },
+      "&::-webkit-scrollbar-track": {
+        background: mode("white", "whiteAlpha.200")(props),
+      },
+      "&::-webkit-scrollbar-thumb": {
+        background: mode("gray.200", "gray.700")(props),
+      },
     },
     "div#__next": {
       height: "-webkit-fill-available",
