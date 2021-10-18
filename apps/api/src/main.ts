c36aff7cd0c0bb875ae67a168924ad011c6e406c
Amos Bastian <amosbastian@gmail.com>
2021-10-18 23:44:49 +0200

    feat(api): add sentry

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,6 @@
 import { createApolloServer, prisma } from "@boilerplate/api/utility";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import * as cookieParser from "cookie-parser";
 import * as cors from "cors";
 import * as express from "express";
@@ -10,6 +12,14 @@ const main = async () => {
 
   const app = express();
 
+  Sentry.init({
+    dsn: "https://91f1ccd1e77948ca928dd670341c9980@o697629.ingest.sentry.io/5989433",
+    integrations: [new Sentry.Integrations.Http({ tracing: true }), new Tracing.Integrations.Express({ app })],
+    tracesSampleRate: 1.0,
+  });
+
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.tracingHandler());
   app.use(cookieParser());
 
   app.use((req, res, next) => {
@@ -24,6 +34,8 @@ const main = async () => {
   app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
   addCreateCheckoutSession(app);
   addCreatePortalLink(app);
+
+  app.use(Sentry.Handlers.errorHandler());
 
   apolloServer.applyMiddleware({
     app,
