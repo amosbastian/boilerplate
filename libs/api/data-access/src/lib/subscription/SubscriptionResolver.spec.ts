c04343875744593dfdc82620a05ee9ad14efa893
Amos Bastian <amosbastian@gmail.com>
2021-10-26 21:35:40 +0200

    test(api): add tests for subscription resolver

---
+++ libs/api/data-access/src/lib/subscription/SubscriptionResolver.spec.ts
@@ -0,0 +1,61 @@
+import { createPrice, createSubscription, createTestContext } from "@boilerplate/api/utility";
+import { gql } from "apollo-server-express";
+
+const ctx = createTestContext();
+
+describe("SubscriptionResolver", () => {
+  describe("subscription", () => {
+    it("should return the subscription matching the given filter", async () => {
+      const subscription = await createSubscription(ctx.prisma);
+
+      const SubscriptionQuery = gql`
+        query Subscription($where: SubscriptionWhereUniqueInput!) {
+          subscription(where: $where) {
+            id
+          }
+        }
+      `;
+
+      const queryResult = await ctx.client.query(SubscriptionQuery, { variables: { where: { id: subscription.id } } });
+
+      expect(queryResult).toEqual({
+        data: {
+          subscription: {
+            id: subscription.id,
+          },
+        },
+      });
+    });
+  });
+
+  describe("price", () => {
+    it("should return the price of the given subscription", async () => {
+      const price = await createPrice(ctx.prisma);
+      const subscription = await createSubscription(ctx.prisma, { price: { connect: { id: price.id } } });
+
+      const SubscriptionPriceQuery = gql`
+        query SubscriptionPrice($where: SubscriptionWhereUniqueInput!) {
+          subscription(where: $where) {
+            price {
+              id
+            }
+          }
+        }
+      `;
+
+      const queryResult = await ctx.client.query(SubscriptionPriceQuery, {
+        variables: { where: { id: subscription.id } },
+      });
+
+      expect(queryResult).toEqual({
+        data: {
+          subscription: {
+            price: {
+              id: price.id,
+            },
+          },
+        },
+      });
+    });
+  });
+});
