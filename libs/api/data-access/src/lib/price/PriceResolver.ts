0f32fc426006c230b0a79c6df841ba20f996f06e
Amos Bastian <amosbastian@gmail.com>
2021-10-26 21:34:19 +0200

    test(api): add tests for price resolver

--- libs/api/data-access/src/lib/price/PriceResolver.ts
+++ libs/api/data-access/src/lib/price/PriceResolver.ts
@@ -1,18 +1,12 @@
 import { Context } from "@boilerplate/shared/types";
-import { FindUniquePriceArgs, Price, Product, UpsertPriceArgs } from "@generated/type-graphql";
-import { Args, Authorized, Ctx, FieldResolver, Mutation, Query, Resolver, Root } from "type-graphql";
+import { FindUniquePriceArgs, Price, Product } from "@generated/type-graphql";
+import { Args, Ctx, FieldResolver, Query, Resolver, Root } from "type-graphql";
 
 @Resolver(() => Price)
 export class PriceResolver {
   @Query(() => Price, { nullable: true })
   async price(@Ctx() { prisma }: Context, @Args() args: FindUniquePriceArgs): Promise<Price | null> {
     return prisma.price.findUnique(args);
-  }
-
-  @Authorized("Admin")
-  @Mutation(() => Price, { nullable: true })
-  async upsertPrice(@Ctx() { prisma }: Context, @Args() args: UpsertPriceArgs): Promise<Price> {
-    return prisma.price.upsert(args);
   }
 
   @FieldResolver(() => Product, { nullable: true })
