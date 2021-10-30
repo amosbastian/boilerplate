52c5aee7a0cc453ad49a19a01a9e2ebd7f0ba4e1
Amos Bastian <amosbastian@gmail.com>
2021-10-30 15:18:59 +0200

    chore(nx): add implicit dependency for i18n with site and blog

--- apps/site/i18n.js
+++ apps/site/i18n.js
@@ -19,9 +19,8 @@ module.exports = {
     "rgx:^/settings": ["settings"],
   },
   loadLocaleFrom: async (locale, namespace) => {
-    const i18nShared = await import(`../../libs/shared/i18n/src/lib/${locale}/common`).then((m) => m.default);
-    const i18nSite = await import(`../../libs/site/i18n/src/lib/${locale}/${namespace}`).then((m) => m.default);
-
-    return { ...i18nShared, ...i18nSite };
+    // const i18nShared = await import(`../../libs/shared/i18n/src/lib/${locale}/common`).then((m) => m.default);
+    // const i18nSite = await import(`../../libs/site/i18n/src/lib/${locale}/${namespace}`).then((m) => m.default);
+    // return { ...i18nShared, ...i18nSite };
   },
 };
