a66298fcae11b70a527265b8049c2adbcb2ce78e
Amos Bastian <amosbastian@gmail.com>
2021-11-17 21:42:19 +0100

    feat: update storybook to work with webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,15 @@ const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
   stories: [],
-  addons: ["storybook-addon-next-router", "@storybook/addon-essentials", "@storybook/addon-a11y"],
+  addons: [
+    "storybook-addon-next-router",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "@nrwl/react/plugins/storybook",
+  ],
+  core: {
+    builder: "webpack5",
+  },
   webpackFinal: async (config) => {
     return {
       ...config,
