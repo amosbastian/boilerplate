3d58f8413ec93ae5578ee45983550b978c37d0ea
Amos Bastian <amosbastian@gmail.com>
2021-11-22 22:31:34 +0100

    fix(api): omit count from return type resolver (temporary)

--- libs/api/data-access/src/lib/price/PriceResolver.ts
+++ libs/api/data-access/src/lib/price/PriceResolver.ts
@@ -5,12 +5,12 @@ import { Args, Ctx, FieldResolver, Query, Resolver, Root } from "type-graphql";
 @Resolver(() => Price)
 export class PriceResolver {
   @Query(() => Price, { nullable: true })
-  async price(@Ctx() { prisma }: Context, @Args() args: FindUniquePriceArgs): Promise<Price | null> {
+  async price(@Ctx() { prisma }: Context, @Args() args: FindUniquePriceArgs): Promise<Omit<Price, "_count"> | null> {
     return prisma.price.findUnique(args);
   }
 
   @FieldResolver(() => Product, { nullable: true })
-  async product(@Root() price: Price, @Ctx() { prisma }: Context): Promise<Product | null> {
+  async product(@Root() price: Price, @Ctx() { prisma }: Context): Promise<Omit<Product, "_count"> | null> {
     return prisma.price
       .findUnique({
         where: {
