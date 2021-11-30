c70602b53904fad26b895244ffb604d816f0da10
Amos Bastian <amosbastian@gmail.com>
2021-11-30 12:55:24 +0100

    chore: add node module caching to ci

--- libs/api/test/src/lib/prisma-test-context/prismaTestContext.ts
+++ libs/api/test/src/lib/prisma-test-context/prismaTestContext.ts
@@ -1,16 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
 export function prismaTestContext() {
-  // const prismaBinary = join(__dirname, "../../../..", "node_modules", ".bin", "prisma");
   let prismaClient: null | PrismaClient = null;
 
   return {
-    async beforeAll() {
-      // Run the migrations to ensure our schema has the required structure
-      // execSync(`${prismaBinary} db push`);
-      // execSync("npx prisma db push");
-    },
-
     async beforeEach() {
       // Construct a new Prisma Client connected to the generated schema
       prismaClient = new PrismaClient();
