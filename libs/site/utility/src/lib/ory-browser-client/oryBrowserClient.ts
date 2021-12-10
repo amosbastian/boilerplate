f692ce9fd3e5200f2f5212c15b7e0449c1a75955
Amos Bastian <amosbastian@gmail.com>
2021-12-10 17:09:18 +0100

    fix(site): update basePath of ory browser client

--- libs/site/utility/src/lib/ory-browser-client/oryBrowserClient.ts
+++ libs/site/utility/src/lib/ory-browser-client/oryBrowserClient.ts
@@ -1,6 +1,7 @@
+import { configuration } from "@boilerplate/shared/configuration";
 import { edgeConfig } from "@ory/integrations/next";
 import { Configuration, V0alpha2Api } from "@ory/kratos-client";
 
 export const oryBrowserClient = new V0alpha2Api(
-  new Configuration({ ...edgeConfig, basePath: "http://localhost:3000/api/.ory" }),
+  new Configuration({ ...edgeConfig, basePath: `${configuration.BASE_URL_SITE}/api/.ory` }),
 );
