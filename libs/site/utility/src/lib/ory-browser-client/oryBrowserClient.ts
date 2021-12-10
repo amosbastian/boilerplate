e595ce31c5310fecca04e6f6e112cc7976e472cd
Amos Bastian <amosbastian@gmail.com>
2021-12-10 17:35:09 +0100

    fix(site): update ory browser client once and for all

--- libs/site/utility/src/lib/ory-browser-client/oryBrowserClient.ts
+++ libs/site/utility/src/lib/ory-browser-client/oryBrowserClient.ts
@@ -2,9 +2,8 @@ import { configuration } from "@boilerplate/shared/configuration";
 import { edgeConfig } from "@ory/integrations/next";
 import { Configuration, V0alpha2Api } from "@ory/kratos-client";
 
+console.log({ ...edgeConfig, basePath: `${configuration.BASE_URL_SITE}/api/.ory` });
+
 export const oryBrowserClient = new V0alpha2Api(
-  new Configuration({
-    ...edgeConfig,
-    ...(process.env.NODE_ENV !== "production" ? { basePath: `${configuration.BASE_URL_SITE}/api/.ory` } : {}),
-  }),
+  new Configuration({ ...edgeConfig, basePath: `${configuration.BASE_URL_SITE}/api/.ory` }),
 );
