15ae4ed3dfc235b2a8b74c0d11d3323a4195776e
Amos <amosbastian@gmail.com>
2022-02-16 00:52:16 +0100

    fix(site): update ory browser client configuration

--- libs/site/utility/src/lib/ory-browser-client/oryBrowserClient.ts
+++ libs/site/utility/src/lib/ory-browser-client/oryBrowserClient.ts
@@ -1,7 +1,4 @@
-import { configuration } from "@boilerplate/shared/configuration";
 import { edgeConfig } from "@ory/integrations/next";
 import { Configuration, V0alpha2Api } from "@ory/kratos-client";
 
-export const oryBrowserClient = new V0alpha2Api(
-  new Configuration({ ...edgeConfig, basePath: `${configuration.BASE_URL_SITE}/api/.ory` }),
-);
+export const oryBrowserClient = new V0alpha2Api(new Configuration(edgeConfig));
